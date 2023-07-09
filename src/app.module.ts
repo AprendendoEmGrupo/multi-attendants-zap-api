5a1bac375365c5ed36858b6e4e15bb92991555d9
Tiago rosa da costa <tiagorosadacost@gmail.com>
2023-07-08 22:26:21 -0300

    Setup queue(bullmq + redis). I commented because no necessary now

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { CommonModule } from './common/common.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
@@ -20,6 +21,17 @@ import { CommonModule } from './common/common.module';
         };
       },
     }),
+    // BullModule.forRoot({
+    //   redis: {
+    //     connectTimeout: 10000,
+    //     username: process.env.REDIS_USER,
+    //     password: process.env.REDIS_PASSWORD,
+    //     tls: {
+    //       host: process.env.REDIS_HOST,
+    //       port: Number(process.env.REDIS_PORT),
+    //     },
+    //   },
+    // }),
     UserModule,
     CommonModule,
   ],
